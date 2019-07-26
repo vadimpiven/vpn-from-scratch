3aae1d0cab03e6b68ae0978e73a5252023fdbceb
Vadim Piven <vadim@piven.tech>
2019-07-26 22:29:51 +0300

    added pipeline

--- reports/007/hw2_signer/signer.go
+++ reports/007/hw2_signer/signer.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"sync"
+)
+
+func ExecutePipeline(jobs ...job) {
+	var prev, curr chan interface{}
+	wg := &sync.WaitGroup{}
+	wg.Add(len(jobs))
+	for _, j := range jobs {
+		curr = make(chan interface{}, MaxInputDataLen)
+		go func(in, out chan interface{}, j job) {
+			defer wg.Done()
+			j(in, out)
+			close(out)
+		}(prev, curr, j)
+		prev = curr
+	}
+	wg.Wait()
+}
+
 func SingleHash(in, out chan interface{}) {
 
 }
@@ -9,9 +29,5 @@ func MultiHash(in, out chan interface{}) {
 }
 
 func CombineResults(in, out chan interface{}) {
-
-}
-
-func ExecutePipeline(jobs ...job) {
 
 }
