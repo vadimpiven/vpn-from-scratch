122b371d88b83841f6cdcf65b5d9c20176dcc069
Vadim Piven <vadim@piven.tech>
2019-08-01 23:59:01 +0400

    prog 4 for report 9 finished

--- reports/009/hw4_test_coverage/client_test.go
+++ reports/009/hw4_test_coverage/client_test.go
@@ -4,45 +4,171 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+	"time"
 )
 
 // SearchServer implements fake external service.
 func SearchServer(w http.ResponseWriter, r *http.Request) {
-
+	defer r.Body.Close()
+	accessToken := r.Header.Get("AccessToken")
+	switch accessToken {
+	case "Timeout":
+		time.Sleep(5 * time.Second)
+		return
+	case "InternalServerError":
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	case "StatusBadRequest1":
+		w.WriteHeader(http.StatusBadRequest)
+		res, _ := json.Marshal(&SearchErrorResponse{"..."})
+		w.Write(res)
+		return
+	case "StatusBadRequest2":
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{,,",`))
+		return
+	case "StatusBadRequest3":
+		w.WriteHeader(http.StatusBadRequest)
+		res, _ := json.Marshal(&SearchErrorResponse{"ErrorBadOrderField"})
+		w.Write(res)
+		return
+	case "InvalidUser":
+		w.Write([]byte(`{,,",`))
+		return
+	case "LimitReached":
+		limit, _ := strconv.Atoi(r.FormValue("limit"))
+		res, _ := json.Marshal(make([]User, limit))
+		w.Write(res)
+		return
+	case "LimitUnreached":
+		res, _ := json.Marshal(make([]User, 10))
+		w.Write(res)
+		return
+	case "ValidToken":
+		break
+	default:
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	//body, err := ioutil.ReadAll(r.Body)
 }
 
 // TestFindUsers runs the tests for FindUsers function of SearchClient struct.
 func TestFindUsers(t *testing.T) {
-	cases := []struct{
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	cases := []struct {
 		Name        string
 		AccessToken string
+		URL         string
 		Request     SearchRequest
 		Response    *SearchResponse
 		IsError     bool
 	}{
 		{
 			Name: "limit must be > 0",
+			URL:  ts.URL,
 			Request: SearchRequest{
 				Limit: -1,
 			},
 			Response: nil,
-			IsError: true,
+			IsError:  true,
 		},
 		{
 			Name: "offset must be > 0",
+			URL:  ts.URL,
 			Request: SearchRequest{
 				Offset: -1,
 			},
 			Response: nil,
-			IsError: true,
+			IsError:  true,
+		},
+		{
+			Name:     "empty",
+			Response: nil,
+			IsError:  true,
+		},
+		{
+			Name:        "unauthorized",
+			AccessToken: "_____",
+			URL:         ts.URL,
+			Response:    nil,
+			IsError:     true,
+		},
+		{
+			Name:        "internal server error",
+			AccessToken: "InternalServerError",
+			URL:         ts.URL,
+			Response:    nil,
+			IsError:     true,
+		},
+		{
+			Name:        "status bad request 1 - unknown",
+			AccessToken: "StatusBadRequest1",
+			URL:         ts.URL,
+			Response:    nil,
+			IsError:     true,
+		},
+		{
+			Name:        "status bad request 2 - broken SearchErrorResponse",
+			AccessToken: "StatusBadRequest2",
+			URL:         ts.URL,
+			Response:    nil,
+			IsError:     true,
+		},
+		{
+			Name:        "status bad request 3 - ErrorBadOrderField",
+			AccessToken: "StatusBadRequest3",
+			URL:         ts.URL,
+			Response:    nil,
+			IsError:     true,
+		},
+		{
+			Name:        "invalid user",
+			AccessToken: "InvalidUser",
+			URL:         ts.URL,
+			Response:    nil,
+			IsError:     true,
+		},
+		{
+			Name:        "limit reached",
+			AccessToken: "LimitReached",
+			URL:         ts.URL,
+			Request: SearchRequest{
+				Limit: 26,
+			},
+			Response: &SearchResponse{
+				NextPage: true,
+				Users:    make([]User, 25),
+			},
+			IsError: false,
+		},
+		{
+			Name:        "limit unreached",
+			AccessToken: "LimitUnreached",
+			URL:         ts.URL,
+			Request: SearchRequest{
+				Limit: 15,
+			},
+			Response: &SearchResponse{
+				NextPage: false,
+				Users:    make([]User, 10),
+			},
+			IsError: false,
+		},
+		{
+			Name:        "timeout",
+			AccessToken: "Timeout",
+			URL:         ts.URL,
+			Response:    nil,
+			IsError:     true,
 		},
 	}
-	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
 	for _, tc := range cases {
 		t.Run(tc.Name, func(t *testing.T) {
 			c := &SearchClient{
-				URL: ts.URL,
+				URL:         tc.URL,
 				AccessToken: tc.AccessToken,
 			}
 			res, err := c.FindUsers(tc.Request)
